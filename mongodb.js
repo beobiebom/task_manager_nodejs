91e5810ec53ee3bb17c42370ae07a9c9bac6ed85
minh duc <69248042+beobiebom@users.noreply.github.com>
2021-07-04 11:16:58 +0700

    Insert documents

--- mongodb.js
+++ mongodb.js
@@ -15,9 +15,55 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: 
     console.log('Connected correctly!')
 
     const db = client.db(databaseName)
+    
+    //Insert one user into database
+    // db.collection('users').insertOne({
+    //     name: 'Minh Duc',
+    //     age: 20
+    // }, (error, result) => {
+    //     if (error) {
+    //         return console.log('Unable to insert user')
+    //     }
+        
+    //     //Print array documents
+    //     console.log(result.ops)
+    // })
 
-    db.collection('users').insertOne({
-        name: 'Minh Duc',
-        age: 20
+    // db.collection('users').insertMany([
+    //     {
+    //         name: 'Jen',
+    //         age: 28
+    //     }, 
+    //     {
+    //         name: 'Gunther',
+    //         age: 27
+    //     }
+    // ], (error, result) => {
+    //     if (error) {
+    //         return console.log('Unable to insert documents')
+    //     }
+
+    //     console.log(result.ops)
+    // })
+
+    db.collection('tasks').insertMany([
+        {
+            description: 'Clean the house',
+            completed: true
+        },
+        {
+            description: 'Renew inspection',
+            completed: false
+        },
+        {
+            description: 'Plot plants',
+            completed: false
+        }
+    ], (error, result) => {
+        if (error) {
+            return console.log('Unable to insert tasks!')
+        }
+
+        console.log(result.ops)
     })
 })
