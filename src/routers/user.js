0bb67fd49e97c2ea3b92cf3ff2cbd314f8c80dc2
minh duc <69248042+beobiebom@users.noreply.github.com>
2021-08-07 23:33:21 +0700

    Logging in user

--- src/routers/user.js
+++ src/routers/user.js
@@ -20,6 +20,15 @@ router.post('/users', async (req, res) => {
 //     })
 })
 
+router.post('/users/login', async (req, res) => {
+    try {
+        const user = await User.findByCredentials(req.body.email, req.body.password) 
+        res.send(user)
+    } catch (error) {
+        res.status(400).send(error)
+    }
+})
+
 router.patch('/users/:id', async (req, res) => {
     const updates = Object.keys(req.body)
     const allowedUpdates = ['name', 'age', 'password']
